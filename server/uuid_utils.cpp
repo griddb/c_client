c17219b0c00cee5b28e137cb28d0647eb5166c0d
knonomura <katsuhiko.nonomura@griddb.org>
2017-04-21 13:59:00 +0900

    add files for C client

---
+++ server/uuid_utils.cpp
@@ -0,0 +1,6 @@
+﻿#include "uuid_utils.h"
+#include "uuid/uuid.h"
+
+void UUIDUtils::generate(UUIDValue &out) {
+	uuid_generate(out);
+}
---
+++ server/uuid_utils.h
@@ -0,0 +1,12 @@
+﻿#ifndef UUID_UTILS_H_
+#define UUID_UTILS_H_
+
+#include "util/type.h"
+
+typedef uint8_t UUIDValue[16];
+
+struct UUIDUtils {
+	static void generate(UUIDValue &out);
+};
+
+#endif
